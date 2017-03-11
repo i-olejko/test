e9bdf9c8bac79c191bd35e8e8e64c6b763b0903c
Oleg <i.olejko@gmail.com>
2017-03-12 01:45:45 +0200

    body parser added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import routes from './routes';
+import bodyParser from 'body-parser'
 
 const app = express();
 
 //middleware 
-
+app.use(bodyParser.json());
 app.use('/api',routes);
 
 export default app;
